c59bffd44556c0c2d287a4276157c09b15829ba3
raulfernandez <raulmelo.web@gmail.com>
2022-06-04 01:50:49 -0300

    Initial project

---
+++ src/stories/Button.stories.ts
@@ -0,0 +1,48 @@
+// also exported from '@storybook/angular' if you can deal with breaking changes in 6.1
+import { Meta, Story } from '@storybook/angular/types-6-0';
+import Button from './button.component';
+
+// More on default export: https://storybook.js.org/docs/angular/writing-stories/introduction#default-export
+export default {
+  title: 'Example/Button',
+  component: Button,
+  // More on argTypes: https://storybook.js.org/docs/angular/api/argtypes
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+  parameters: {
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/file/1gB8omUWhALAmIBjSpXQOo/SYZ-2021?node-id=1076%3A7408',
+    }
+  }
+} as Meta;
+
+// More on component templates: https://storybook.js.org/docs/angular/writing-stories/introduction#using-args
+const Template: Story<Button> = (args: Button) => ({
+  props: args,
+});
+
+export const Primary = Template.bind({});
+
+Primary.args = {
+  primary: true,
+  label: 'Button',
+};
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+  label: 'Button',
+};
+
+export const Large = Template.bind({});
+Large.args = {
+  size: 'large',
+  label: 'Button',
+};
+
+export const Small = Template.bind({});
+Small.args = {
+  size: 'small',
+  label: 'Button',
+};
